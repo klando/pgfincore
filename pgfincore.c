cbf705e03a4872aa24213892a25b1a4d9d0c403f
Cédric Villemain <cedric.villemain.debian@gmail.com>
2009-06-27 22:19:50 +0200

    Functions working \o/

--- pgfincore.c
+++ pgfincore.c
@@ -24,19 +24,6 @@
 # Dave Plonka, Apr  5 2007
 */
 
-
-#include "postgres.h" /* general Postgres declarations */
-
-
-
-#include "mb/pg_wchar.h"
-#include "utils/elog.h"
-#include "utils/builtins.h"
-
-#ifdef PG_MODULE_MAGIC
-PG_MODULE_MAGIC;
-#endif
-
 /* { POSIX stuff */
 #include <errno.h> /* errno */
 #include <fcntl.h> /* fcntl, open */
@@ -48,30 +35,118 @@ PG_MODULE_MAGIC;
 #include <unistd.h> /* sysconf, close */
 /* } */
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include "postgres.h" /* general Postgres declarations */
+
+#include "mb/pg_wchar.h"
+#include "utils/elog.h"
+#include "utils/builtins.h"
+
 #include <sys/mman.h>
 
-Datum pgfincore(PG_FUNCTION_ARGS); /* Prototype */
+#include "access/heapam.h"
+#include "catalog/catalog.h"
+#include "catalog/namespace.h"
+#include "catalog/pg_tablespace.h"
+#include "commands/dbcommands.h"
+#include "commands/tablespace.h"
+#include "miscadmin.h"
+#include "storage/fd.h"
+#include "utils/acl.h"
+#include "utils/syscache.h"
+#include "utils/relcache.h"
+#include "utils/rel.h"
+// #include "pg_config_manual.h"
 
-PG_FUNCTION_INFO_V1(pgfincore);
+#ifdef PG_MODULE_MAGIC
+PG_MODULE_MAGIC;
+#endif
+
+
+
+Datum pgfincore_name(PG_FUNCTION_ARGS); /* Prototype */
+Datum pgfincore_oid(PG_FUNCTION_ARGS); /* Prototype */
+static int64 pgfincore_all(RelFileNode *rfn);
+static int64 pgfincore(char *filename);
+
+PG_FUNCTION_INFO_V1(pgfincore_name);
+PG_FUNCTION_INFO_V1(pgfincore_oid);
+
+
 
 /* fincore -
  */
-Datum pgfincore(PG_FUNCTION_ARGS) {
-  VarChar *str_in     = (VarChar *)PG_GETARG_VARCHAR_P(0);
-  int     str_in_size = VARSIZE(str_in) - VARHDRSZ;
-  char    *filename;
-  VarChar *return_pgfincore;
-  int n=0;
+Datum
+pgfincore_oid(PG_FUNCTION_ARGS)
+{
+	Oid			relOid = PG_GETARG_OID(0);
+	Relation	rel;
+	int64		size = 0;
 
-  char    *output;
+	rel = relation_open(relOid, AccessShareLock);
+
+	size = pgfincore_all(&(rel->rd_node));
+
+	relation_close(rel, AccessShareLock);
+
+	PG_RETURN_INT64(size);
+}
+Datum pgfincore_name(PG_FUNCTION_ARGS) {
+	text	   *relname = PG_GETARG_TEXT_P(0);
+	RangeVar   *relrv;
+	Relation	rel;
+	int64		size = 0;
+
+	relrv = makeRangeVarFromNameList(textToQualifiedNameList(relname));
+	rel = relation_openrv(relrv, AccessShareLock);
+
+	size = pgfincore_all(&(rel->rd_node));
+	elog(DEBUG1, "pgfincore3 : %lu",(unsigned long)size);
+
+	relation_close(rel, AccessShareLock);
+
+	return size;
+}
+
+// calculate number of block in memory
+static int64
+pgfincore_all(RelFileNode *rfn)
+{
+	int64		totalsize = 0;
+	int64		result    = 0;
+	char	   *relationpath;
+	char		pathname[MAXPGPATH];
+	unsigned int segcount = 0;
+
+	relationpath = relpath(*rfn);
+
+	for (segcount = 0;; segcount++)
+	{
+		if (segcount == 0)
+			snprintf(pathname, MAXPGPATH, "%s",
+					 relationpath);
+		else
+			snprintf(pathname, MAXPGPATH, "%s.%u",
+					 relationpath, segcount);
+
+		result = pgfincore(pathname);
+	elog(DEBUG1, "pgfincore : %lu",(unsigned long)result);
+
+		if (result == -1)
+		  break;
+		totalsize += result;
+	}
+	elog(DEBUG1, "pgfincore2 : %lu",(unsigned long)totalsize);
+
+	return (unsigned long)totalsize;
+}
 
 
-
-
+static int64
+pgfincore(char *filename) {
+  // our counter for block in memory
+  int     n=0;
+  // our return value TODO use array.
+//   VarChar *return_pgfincore ;
 
   // for open file
   int fd;
@@ -80,94 +155,67 @@ Datum pgfincore(PG_FUNCTION_ARGS) {
   // for mmap file
   void *pa = (char *)0;
   // for calloc file
-  char *vec = (char *)0;
+  unsigned char *vec = (unsigned char *)0;
 
-  size_t pageSize = getpagesize();
+  // OS things
+  off_t pa_offset;
+  size_t pageSize = sysconf(_SC_PAGESIZE);
+  register size_t pageIndex;
 
+/* Do the main work */
+  fd = open(filename, O_RDONLY);
+  if (fd == -1)
+	return -1;
+//     elog(ERROR, "Can not open object file : %s", filename);
 
-   register size_t pageIndex;
-
-
-
-
-
-
-
-  pg_verifymbstr(VARDATA(str_in), str_in_size, false);
-
-  filename = DatumGetCString(
-                DirectFunctionCall1(textout,
-                  PointerGetDatum(str_in)
-                )
-              );
-  PG_FREE_IF_COPY(str_in, 0);
-
-
-
-
-
-
-
-
-  fd = open(filename, 0);
-  if (0 > fd) {
-    elog(ERROR, "Can not open object file : %s", filename);
-    return 0;
-  }
-
-  if (0 != fstat(fd, &st)) {
+  if (fstat(fd, &st) == -1) {
+    close(fd);
     elog(ERROR, "Can not stat object file : %s", filename);
-    close(fd);
-    return 0;
   }
 
-  pa = mmap((void *)0, st.st_size, PROT_NONE, MAP_SHARED, fd, 0);
-  if (MAP_FAILED == pa) {
+  pa_offset = 0 & ~(sysconf(_SC_PAGE_SIZE) - 1);
+
+  pa = mmap(NULL, st.st_size - pa_offset, PROT_NONE, MAP_SHARED, fd, pa_offset);
+  if (pa == MAP_FAILED) {
+    close(fd);
     elog(ERROR, "Can not mmap object file : %s", filename);
-    close(fd);
-    return 0;
   }
 
-  /* vec = calloc(1, 1+st.st_size/pageSize); */
   vec = calloc(1, (st.st_size+pageSize-1)/pageSize);
   if ((void *)0 == vec) {
-    elog(ERROR, "Can not calloc object file : %s", filename);
+    munmap(pa, (st.st_size+pageSize-1)/pageSize);
     close(fd);
-    return 0;
+    elog(ERROR, "Can not calloc object file : %s", filename);
+    return -1;
   }
 
   if (0 != mincore(pa, st.st_size, vec)) {
-    /* perror("mincore"); */
+    free(vec);
+    munmap(pa, (st.st_size+pageSize-1)/pageSize);
+    close(fd);
     elog(ERROR, "mincore(%p, %lu, %p): %s\n",
             pa, (unsigned long)st.st_size, vec, strerror(errno));
-    free(vec);
-    close(fd);
-    return 0;
+    return -1;
   }
 
   /* handle the results */
   for (pageIndex = 0; pageIndex <= st.st_size/pageSize; pageIndex++) {
-    if (vec[pageIndex]&1) {
+	// block in memory
+    if (vec[pageIndex] & 1) {
       n++;
 //       elog (NOTICE, "r: %lu / %lu", (unsigned long)pageIndex, (unsigned long)(st.st_size/pageSize));  /* TODO fix that /!\ (on veut concaténer tous les résultats pour le moment)*/
     }
   }
 
+//   free things
   free(vec);
-  vec = (char *)0;
-  munmap(pa, st.st_size);
+  munmap(pa, (st.st_size+pageSize-1)/pageSize);
   close(fd);
 
-  elog(ERROR, "pgfincore : %lu of %lu block in linux cache",(unsigned long)n, (unsigned long)(st.st_size/pageSize));
-  /* convert string to VarChar for result */
-  return_pgfincore = DatumGetVarCharP(
-                  DirectFunctionCall2(
-                    varcharin,
-                    CStringGetDatum(output),
-                    Int32GetDatum(strlen(output) + VARHDRSZ)
-                  )
-                );
+//   return_pgfincore = sprintf("pgfincore : %lu of %lu block in linux cache",(unsigned long)n, (unsigned long)(st.st_size/pageSize));
 
- PG_RETURN_VARCHAR_P(return_pgfincore);
+	elog(DEBUG1, "pgfincore : %lu of %lu block in linux cache",(unsigned long)n, (unsigned long)(st.st_size/pageSize));
+
+//   PG_RETURN_VARCHAR_P(return_pgfincore);
+  return (unsigned long)n;
 }
-
