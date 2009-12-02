3afe4a3a32aee1abe052627918f5bc250791746b
Cédric Villemain <cedric.villemain.debian@gmail.com>
2009-12-02 01:51:24 +0100

    fix copyright

--- pgfincore.c
+++ pgfincore.c
@@ -1,23 +1,7 @@
 /*
 *  PgFincore
 *  This project let you see what objects are in the FS cache memory
-*  Copyright (C) 2009 Cédric Villemain Dalibo
-*
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*
-*
+*  Copyright (C) 2009 Cédric Villemain
 */
 
 /* { POSIX stuff */
