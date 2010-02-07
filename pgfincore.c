7f746c45bc3852db1e3dc02f684f750f97306231
Cédric Villemain <cedric.villemain.debian@gmail.com>
2010-02-07 04:05:58 +0100

    errno to catch the last segment

--- pgfincore.c
+++ pgfincore.c
@@ -96,7 +96,6 @@ pgfincore(PG_FUNCTION_ARGS)
   pgfincore_fctx  *fctx;
   Datum 		  result;
   char			  pathname[MAXPGPATH];
-  bool 			  isnull;
 
   /* stuff done only on the first call of the function */
   if (SRF_IS_FIRSTCALL())
@@ -190,8 +189,7 @@ pgfincore(PG_FUNCTION_ARGS)
   * When we have work with all segment of the current relation, test success
   * We exit from the SRF
   */
-  if (DatumGetInt64(GetAttributeByName((HeapTupleHeader)result, "block_disk", &isnull)) == 0
-	|| isnull )
+  if (errno == ENOENT )
   {
 	relation_close(fctx->rel, AccessShareLock);
 	pfree(fctx);
@@ -359,7 +357,6 @@ pgmincore_file(char *filename, int action, FunctionCallInfo fcinfo)
   free(vec);
   munmap(pa, st.st_size);
   close(fd);
-
   return HeapTupleGetDatum(tuple); /* return filename, block_disk, block_mem, group_mem   */
 
 error:
@@ -370,6 +367,7 @@ error:
   values[4] = Int64GetDatum(false);
   memset(nulls, 0, sizeof(nulls));
   tuple = heap_form_tuple(tupdesc, values, nulls);
+  errno = ENOENT;
   return (HeapTupleGetDatum(tuple));
 }
 
@@ -472,6 +470,7 @@ error:
   values[3] = Int64GetDatum(false);
   memset(nulls, 0, sizeof(nulls));
   tuple = heap_form_tuple(tupdesc, values, nulls);
+  errno = ENOENT;
   return (HeapTupleGetDatum(tuple));
 }
 
@@ -507,7 +506,7 @@ pgfadv_snapshot(char *filename, int fd, int action)
 	  if (file == NULL)
 	  {
 		  if (errno == ENOENT)
-			  return;				/* ignore not-found error */
+			  return block_mem;				/* ignore not-found error */
 		  goto error;
 	  }
 
@@ -553,4 +552,5 @@ error:
 		FreeFile(file);
 	/* If possible, throw away the bogus file; ignore any error */
 	unlink(path);
+	return block_mem;
 }
