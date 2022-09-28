e73337173c34b8dacb9a95fe59af411fec0385da
Christoph Berg <myon@debian.org>
2022-09-28 14:10:15 +0200

    Fix regression tests for PG 15
    In PG 15, "select true" no longer yields "bool" as "?column?" as column
    name. Fix by omitting the target list altogether; the function is still
    called as witnessed by the '(1 row)' output.

--- sql/pgfincore.sql
+++ sql/pgfincore.sql
@@ -3,8 +3,8 @@ CREATE EXTENSION pgfincore;
 --
 -- test SYSCONF
 --
-select true from pgsysconf();
-select true from pgsysconf_pretty();
+select from pgsysconf();
+select from pgsysconf_pretty();
 
 --
 -- make a temp table to use below
@@ -19,33 +19,33 @@ CREATE TEMP TABLE test AS SELECT generate_series(1,256) as a;
 --
 -- test fadvise_loader
 --
-select true from pgfadvise_loader('test', 0, true, true, B'1010');
-select true from pgfadvise_loader('test', 0, true, false, B'1010');
-select true from pgfadvise_loader('test', 0, false, true, B'1010');
-select true from pgfadvise_loader('test', 0, false, false, B'1010');
+select from pgfadvise_loader('test', 0, true, true, B'1010');
+select from pgfadvise_loader('test', 0, true, false, B'1010');
+select from pgfadvise_loader('test', 0, false, true, B'1010');
+select from pgfadvise_loader('test', 0, false, false, B'1010');
 -- must not fail on empty databit input
-select true from pgfadvise_loader('test', 0, false, false, B'');
+select from pgfadvise_loader('test', 0, false, false, B'');
 -- ERROR on NULL databit input
-select true from pgfadvise_loader('test', 0, false, false, NULL);
+select from pgfadvise_loader('test', 0, false, false, NULL);
 
 --
 -- test pgfincore
 --
-select true from pgfincore('test', true);
-select true from pgfincore('test');
+select from pgfincore('test', true);
+select from pgfincore('test');
 
 --
 -- test DONTNEED, WILLNEED
 --
-select true from pgfadvise_willneed('test');
-select true from pgfadvise_dontneed('test');
+select from pgfadvise_willneed('test');
+select from pgfadvise_dontneed('test');
 
 --
 -- test PGFADVISE flags
 --
-select true from pgfadvise_sequential('test');
-select true from pgfadvise_random('test');
-select true from pgfadvise_normal('test');
+select from pgfadvise_sequential('test');
+select from pgfadvise_random('test');
+select from pgfadvise_normal('test');
 
 --
 -- tests drawers
