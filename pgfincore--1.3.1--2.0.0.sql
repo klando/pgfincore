12d06ce7cb6848c94ee570c8c1be331eb48a6c8f
Cédric Villemain <Cedric.Villemain@Data-Bene.io>
2023-12-01 20:26:42 +0100

    Add linux 6.5 cachestat syscall
    Linux 6.5 introduced sys_cachestat:
    https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/mm/filemap.c?h=v6.5#n4174
    
    This commit adds a vm_cachestat() which allows to execute cachestat() on
    all or a part of a relation, it returns a single tuple with a summary
    for all underlying relevant files.
    
    Getting per file stats is possible by providing accurate starting block.

--- pgfincore--1.3.1--2.0.0.sql
+++ pgfincore--1.3.1--2.0.0.sql
@@ -45,3 +45,58 @@ IS 'pgsysconf is DEPRECATED, use pg_sysconf_size and vm_free_pages instead.';
 
 COMMENT ON FUNCTION pgsysconf_pretty()
 IS 'pgsysconf_pretty is DEPRECATED, use pg_sysconf_size and vm_free_pages with pg_size_pretty';
+
+CREATE FUNCTION
+vm_cachestat(
+  IN relation regclass
+, IN fork_name text
+, IN block_num bigint
+, IN nblocks bigint
+, OUT vm_pagesize bigint
+, OUT nr_cache bigint
+, OUT nr_dirty bigint
+, OUT nr_writeback bigint
+, OUT nr_evicted bigint
+, OUT nr_recently_evicted bigint
+, OUT pg_pagesize bigint
+)
+RETURNS record
+AS '$libdir/pgfincore'
+LANGUAGE C;
+
+CREATE FUNCTION
+vm_cachestat(
+  IN relation regclass
+, IN block_num bigint
+, IN nblocks bigint
+, OUT vm_pagesize bigint
+, OUT nr_cache bigint
+, OUT nr_dirty bigint
+, OUT nr_writeback bigint
+, OUT nr_evicted bigint
+, OUT nr_recently_evicted bigint
+, OUT pg_pagesize bigint
+)
+RETURNS record
+AS $vm_cachestat$
+SELECT * FROM vm_cachestat($1,'main',$2,$3)
+$vm_cachestat$
+LANGUAGE SQL;
+
+CREATE FUNCTION
+vm_cachestat(
+  IN relation regclass
+, OUT vm_pagesize bigint
+, OUT nr_cache bigint
+, OUT nr_dirty bigint
+, OUT nr_writeback bigint
+, OUT nr_evicted bigint
+, OUT nr_recently_evicted bigint
+, OUT pg_pagesize bigint
+)
+RETURNS record
+AS $vm_cachestat$
+SELECT * FROM vm_cachestat($1,'main',NULL,NULL)
+$vm_cachestat$
+LANGUAGE SQL;
+
--- sql/pgfincore.sql
+++ sql/pgfincore.sql
@@ -65,3 +65,22 @@ select pg_page_size, sys_pages_size from pg_sysconf_size();
 -- tests vm_free_pages
 --
 select from vm_free_pages();
+
+--
+-- tests vm_cachestat
+--
+-- Test bad parameters
+select * from vm_cachestat(NULL, NULL, -1, -1);
+select * from vm_cachestat('test', NULL, -1, -1);
+select * from vm_cachestat('test', 'vm', -1, -1);
+select * from vm_cachestat('test', 'main', -1, -1);
+select * from vm_cachestat('test', 'main', NULL, -1);
+select * from vm_cachestat('test', 'main', 10, NULL);
+select * from vm_cachestat('test', 'main', 0, 10);
+-- Working cases
+select from pgfadvise_loader('test', 0, true, true, B'1111');
+select (pgfincore('test')).pages_mem, (vm_cachestat('test', 'main', NULL, NULL)).nr_cache;
+select from pgfadvise_loader('test', 0, true, true, B'1000');
+select (pgfincore('test')).pages_mem, (vm_cachestat('test', 'main', NULL, NULL)).nr_cache;
+select from pgfadvise_loader('test', 0, true, true, B'0000');
+select (pgfincore('test')).pages_mem, (vm_cachestat('test', 'main', NULL, NULL)).nr_cache;
