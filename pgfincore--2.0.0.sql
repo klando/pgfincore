47f5d4ebe428d45d755c5f111c98ce4da652a2fe
Cédric Villemain <Cedric.Villemain@Data-Bene.io>
2023-12-01 19:46:30 +0100

    Add pg_sysconf_size, vm_free_size, drop pgsysconf
    pg_sysconf_size returns 'immutable' sizes from sysconf and pg:
    - postgres page size
    - system page size
    - total memory
    vm_free_size returns the current free memory on the system
    
    Remove pgsysconf datum which is obsolete, and add an SQL wrapper on top
    of the new functions.
    Users are encouraged to update their code using pgsysconf which is now
    deprecated and will be removed in the future.

--- pgfincore--2.0.0.sql
+++ pgfincore--2.0.0.sql
@@ -1,24 +1,52 @@
 --
 -- SYSCONF
 --
-CREATE OR REPLACE FUNCTION
+CREATE FUNCTION
+pg_sysconf_size(
+  OUT pg_page_size    bigint
+, OUT sys_pages_size  bigint
+, OUT sys_total_pages bigint
+)
+RETURNS record
+AS '$libdir/pgfincore'
+LANGUAGE C IMMUTABLE;
+
+COMMENT ON FUNCTION pg_sysconf_size()
+IS 'Get system information:
+ - pg_page_size is PostgreSQL page size
+ - sys_pages_size is system page size
+ - sys_total_pages is total memory pages';
+
+CREATE FUNCTION
+vm_free_pages(
+  OUT sys_pages_free    bigint
+)
+RETURNS bigint
+AS '$libdir/pgfincore'
+LANGUAGE C STABLE;
+
+COMMENT ON FUNCTION vm_free_pages()
+IS 'Get system information:
+ - sys_pages_free is current number of free pages in system memory';
+
+CREATE FUNCTION
 pgsysconf(OUT os_page_size   bigint,
           OUT os_pages_free  bigint,
           OUT os_total_pages bigint)
 RETURNS record
-AS '$libdir/pgfincore'
-LANGUAGE C;
+AS $pgsysconf$
+SELECT
+  p.sys_pages_size as os_page_size
+, v.sys_pages_free as os_pages_free
+, p.sys_total_pages as os_total_pages
+FROM pg_sysconf_size() p, vm_free_pages() v
+$pgsysconf$
+LANGUAGE SQL STABLE;
 
 COMMENT ON FUNCTION pgsysconf()
-IS 'Get system configuration information at run time:
- - os_page_size is _SC_PAGESIZE 
- - os_pages_free is _SC_AVPHYS_PAGES
- - os_total_pages is _SC_PHYS_PAGES
+IS 'pgsysconf is DEPRECATED, use pg_sysconf_size and vm_free_pages instead.';
 
-man 3 sysconf for details';
-
-
-CREATE OR REPLACE FUNCTION
+CREATE FUNCTION
 pgsysconf_pretty(OUT os_page_size   text,
                  OUT os_pages_free  text,
                  OUT os_total_pages text)
@@ -31,7 +59,7 @@ from pgsysconf()'
 LANGUAGE SQL;
 
 COMMENT ON FUNCTION pgsysconf_pretty()
-IS 'Pgsysconf() with human readable output';
+IS 'pgsysconf_pretty is DEPRECATED, use pg_sysconf_size and vm_free_pages with pg_size_pretty';
 
 --
 -- PGFADVISE
