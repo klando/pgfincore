3551c1e13b837f1efaf9d266d5b87f5995146de1
Cédric Villemain <Cedric.Villemain@Data-Bene.io>
2023-12-04 01:20:49 +0100

    Add SQL for vm_cachestat
    into pgfincore 2.0.0

--- pgfincore--2.0.0.sql
+++ pgfincore--2.0.0.sql
@@ -191,3 +191,60 @@ LANGUAGE C;
 
 COMMENT ON FUNCTION pgfincore_drawer(varbit)
 IS 'A naive drawing function to visualize page cache per object';
+
+/*
+ * New functions
+ */
+CREATE FUNCTION
+vm_cachestat(
+  IN relation regclass
+, IN fork_name text
+, IN block_num bigint
+, IN nblocks bigint
+, OUT vm_pagesize bigint
+, OUT nr_cache bigint
+, OUT nr_dirty bigint
+, OUT nr_writeback bigint
+, OUT nr_evicted bigint
+, OUT nr_recently_evicted bigint
+, OUT pg_pagesize bigint
+)
+RETURNS record
+AS '$libdir/pgfincore'
+LANGUAGE C;
+
+CREATE FUNCTION
+vm_cachestat(
+  IN relation regclass
+, IN block_num bigint
+, IN nblocks bigint
+, OUT vm_pagesize bigint
+, OUT nr_cache bigint
+, OUT nr_dirty bigint
+, OUT nr_writeback bigint
+, OUT nr_evicted bigint
+, OUT nr_recently_evicted bigint
+, OUT pg_pagesize bigint
+)
+RETURNS record
+AS $vm_cachestat$
+SELECT * FROM vm_cachestat($1,'main',$2,$3)
+$vm_cachestat$
+LANGUAGE SQL;
+
+CREATE FUNCTION
+vm_cachestat(
+  IN relation regclass
+, OUT vm_pagesize bigint
+, OUT nr_cache bigint
+, OUT nr_dirty bigint
+, OUT nr_writeback bigint
+, OUT nr_evicted bigint
+, OUT nr_recently_evicted bigint
+, OUT pg_pagesize bigint
+)
+RETURNS record
+AS $vm_cachestat$
+SELECT * FROM vm_cachestat($1,'main',NULL,NULL)
+$vm_cachestat$
+LANGUAGE SQL;
